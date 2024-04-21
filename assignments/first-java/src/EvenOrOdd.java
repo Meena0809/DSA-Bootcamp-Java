271ccc95c74bc55619cadb53a871a481a2dbea71
Meena <meenapeddammala@gmail.com>
2024-04-21 12:14:21 +0530

    even or odd program

---
+++ assignments/first-java/src/EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter any number");
+        int n = sc.nextInt();
+        if(n%2 == 0)
+        {
+            System.out.println("n is even");
+        }
+        else {
+            System.out.println("n is odd");
+        }
+    }
+}
